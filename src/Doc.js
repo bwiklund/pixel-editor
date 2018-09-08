de86c85384167d60655ceed556f111826d2661c3
Ben Wiklund <ben@daisyowl.com>
2018-09-08 14:50:53 -0700

    loading

--- src/Doc.js
+++ src/Doc.js
@@ -20,33 +20,17 @@ export class DocView extends React.Component {
     const doc = this.props.doc;
     const ctx = this.refs.canvas.getContext("2d")
 
-    const img = new Image();
-    img.src = "peepAvatar_neutral_0.png"; // TODO pull this out when i have more coherent plan for loading images
-    img.onload = () => {
-      // make a headless canvas to parse the png into pixels for us. this is not the canvas we draw the editor image to!
-      let imageLoaderCanvas = document.createElement("canvas");
-      imageLoaderCanvas.width = img.width;
-      imageLoaderCanvas.height = img.height;
-      let loaderCtx = imageLoaderCanvas.getContext('2d');
-      loaderCtx.drawImage(img, 0, 0);
-      let loaderData = loaderCtx.getImageData(0, 0, img.width, img.height);
-      doc.pixels = loaderData.data;
-      doc.width = img.width;
-      doc.height = img.height;
-      // end messy loading stuff to be moved elsewhere...
-
-      let idata = ctx.getImageData(0, 0, doc.width, doc.height);
-      for (let y = 0; y < doc.height; y++) {
-        for (let x = 0; x < doc.width; x++) {
-          var i = x + y * doc.width;
-          var I = i * 4;
-          idata.data[I + 0] = doc.pixels[I + 0];
-          idata.data[I + 1] = doc.pixels[I + 1];
-          idata.data[I + 2] = doc.pixels[I + 2];
-          idata.data[I + 3] = doc.pixels[I + 3];
-        }
+    let idata = ctx.getImageData(0, 0, doc.width, doc.height);
+    for (let y = 0; y < doc.height; y++) {
+      for (let x = 0; x < doc.width; x++) {
+        var i = x + y * doc.width;
+        var I = i * 4;
+        idata.data[I + 0] = doc.pixels[I + 0];
+        idata.data[I + 1] = doc.pixels[I + 1];
+        idata.data[I + 2] = doc.pixels[I + 2];
+        idata.data[I + 3] = doc.pixels[I + 3];
       }
-      ctx.putImageData(idata, 0, 0);
     }
+    ctx.putImageData(idata, 0, 0);
   }
 }
--- src/index.js
+++ src/index.js
@@ -8,8 +8,33 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      docs: [new Doc(50, 70)]
+      docs: []
     };
+  }
+
+  componentDidMount() {
+    this.newDocFromImage("peepAvatar_neutral_0.png");
+  }
+
+  newDocFromImage(path) {
+    const img = new Image();
+    img.src = path; // TODO pull this out when i have more coherent plan for loading images
+    img.onload = () => {
+      // make a headless canvas to parse the png into pixels for us. this is not the canvas we draw the editor image to!
+      let imageLoaderCanvas = document.createElement("canvas");
+      imageLoaderCanvas.width = img.width;
+      imageLoaderCanvas.height = img.height;
+      let loaderCtx = imageLoaderCanvas.getContext('2d');
+      loaderCtx.drawImage(img, 0, 0);
+      let loaderData = loaderCtx.getImageData(0, 0, img.width, img.height);
+
+      let doc = new Doc(img.width, img.height);
+      doc.pixels = loaderData.data;
+      // end messy loading stuff to be moved elsewhere...
+      this.setState({
+        docs: this.state.docs.concat(doc)
+      })
+    }
   }
 
   render() {
