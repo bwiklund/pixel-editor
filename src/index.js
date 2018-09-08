064059e35331819e567aff995fa37be7efb31d95
Ben Wiklund <ben@daisyowl.com>
2018-09-08 14:08:59 -0700

    app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      docs: []
+    };
+  }
+
+  render() {
+    return <div>{"I'm an app"}</div>
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
