030112c1079d7a92d23e9ba126e49fe03246c76f
Ben Wiklund <ben@daisyowl.com>
2018-09-08 15:29:30 -0700

    pentest

--- src/Doc.js
+++ src/Doc.js
@@ -1,10 +1,56 @@
 import React from 'react';
 
+export class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(o) {
+    return new Vec(this.x + o.x, this.y + o.y);
+  }
+
+  sub(o) {
+    return new Vec(this.x - o.x, this.y - o.y);
+  }
+
+  mag() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+
+  scalarMult(n) {
+    return new Vec(this.x * n, this.y * n);
+  }
+}
+
 export class Doc {
   constructor(width, height) {
     this.width = width;
     this.height = height;
     this.pixels = new Array(width * height * 4).fill(128);
+  }
+
+  setPixel(v, r, g, b, a) {
+    var i = ~~v.x + ~~v.y * this.width;
+    var I = i * 4;
+    this.pixels[I + 0] = r;
+    this.pixels[I + 1] = g;
+    this.pixels[I + 2] = b;
+    this.pixels[I + 3] = a;
+  }
+
+  drawLine(p1, p2, r, g, b, a) {
+    // TODO: move this to a pencil tool class
+    // first pass, just move <= 1 pixel at a time in the right direction.
+    // this could be more efficient if i cared
+    const offset = p2.sub(p1);
+    const dist = offset.mag();
+    const stepSize = 0.1;
+    for (let n = 0; n <= dist; n += stepSize) {
+      const p = p1.add(offset.scalarMult(n / dist));
+
+      this.setPixel(p, r, g, b, a);
+    }
   }
 }
 
@@ -39,19 +85,20 @@ export class DocView extends React.Component {
   }
 
   onMouseDown(e) {
-    let fx = (e.pageX - this.refs.canvas.offsetLeft) / this.state.zoom;
-    let fy = (e.pageY - this.refs.canvas.offsetTop) / this.state.zoom;
-
-    var x = Math.floor(fx);
-    var y = Math.floor(fy);
+    let pos = new Vec(
+      (e.pageX - this.refs.canvas.offsetLeft) / this.state.zoom,
+      (e.pageY - this.refs.canvas.offsetTop) / this.state.zoom
+    );
 
     var doc = this.props.doc;
-    var i = x + y * doc.width;
-    var I = i * 4;
-    doc.pixels[I+0] = 0;
-    doc.pixels[I+1] = 0;
-    doc.pixels[I+2] = 0;
-    doc.pixels[I+3] = 255;
+
+    // TODO: lastpos is a janky test of line drawing, move this plz
+    if (!this.lastPos) {
+      doc.setPixel(pos, 0, 0, 0, 255);
+    } else {
+      doc.drawLine(pos, this.lastPos, 0, 0, 0, 255);
+    }
+    this.lastPos = pos;
 
     this.redraw();
   }
