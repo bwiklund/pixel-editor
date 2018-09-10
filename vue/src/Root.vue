45f159b8dfd03abcedc9dd063f75a9fc9f8e66e8
Ben Wiklund <ben@daisyowl.com>
2018-09-10 10:34:28 -0700

    hi

---
+++ vue/src/Root.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <AppView v-bind:app="app"/>
+  </div>
+</template>
+
+<script>
+import AppView from './components/AppView.vue'
+
+class App {
+  constructor() {
+    this.a = 0;
+  }
+
+  foo() {
+    this.a++;
+  }
+}
+
+export default {
+  name: 'root',
+  components: {
+    AppView
+  },
+  data: function() {
+    return {
+      app: new App()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/components/AppView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>Count: {{ app.a }}</h1>
+    <a v-on:click="app.foo()">More</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppView",
+  props: ["app"]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
