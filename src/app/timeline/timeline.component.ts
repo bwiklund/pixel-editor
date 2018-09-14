2706dd882e31ecb51aa5e91ff6bb40d37c3c4572
Ben Wiklund <ben@daisyowl.com>
2018-09-13 17:10:43 -0700

    hmmmmmmmm

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -17,6 +17,9 @@ export class TimelineComponent implements OnInit {
 
   selection: Layer[] = [];
   dragDropIndex: number = -1;
+  isMouseDown: boolean = false;
+  isDragging: boolean = false;
+  dragStartedOnLayer: Layer = null;
 
   constructor(public inputState: InputStateService) { }
 
@@ -25,16 +28,8 @@ export class TimelineComponent implements OnInit {
 
   mousedown(e: MouseEvent, layer: Layer) {
     this.inputState.takeFocus(this);
-
-    if (e.shiftKey) {
-      if (!this.selection.includes(layer)) {
-        this.selection.push(layer);
-      } else {
-        this.selection.splice(this.selection.indexOf(layer), 1);
-      }
-    } else {
-      this.selection = [layer];
-    }
+    this.isMouseDown = true;
+    this.dragStartedOnLayer = layer;
   }
 
   mouseEventPositionInElementNormalized(e: MouseEvent, el: Element) {
@@ -46,10 +41,16 @@ export class TimelineComponent implements OnInit {
   mousemove(e: MouseEvent, layer: Layer) {
     // NOTE: this can come in with/without layer undefined, meaning it's an event from outside the div...
     if (this.inputState.hasFocus(this)) {
-      if (layer) {
-        this.dragDropIndex = this.doc.layers.indexOf(layer);
-        if (this.mouseEventPositionInElementNormalized(e, <Element>e.target).y > 0.5) {
-          this.dragDropIndex += 1;
+      if (this.isMouseDown) {
+        this.isDragging = true;
+      }
+
+      if (this.isDragging) {
+        if (layer) {
+          this.dragDropIndex = this.doc.layers.indexOf(layer);
+          if (this.mouseEventPositionInElementNormalized(e, <Element>e.target).y > 0.5) {
+            this.dragDropIndex += 1;
+          }
         }
       }
     }
@@ -57,40 +58,83 @@ export class TimelineComponent implements OnInit {
 
   @HostListener('window:mouseup', ['$event'])
   mouseup(e: MouseEvent, layer: Layer) {
+    this.isMouseDown = false;
     // NOTE: this can come in with/without layer undefined, meaning it's an event from outside the div...
     if (this.inputState.hasFocus(this)) {
-      if (layer) {
-        this.doc.historyPush("Rearrange Layers");
-
-        var targetIndexInNewLayers = this.doc.layers.indexOf(layer);
-        if (this.mouseEventPositionInElementNormalized(e, <Element>e.target).y > 0.5) {
-          targetIndexInNewLayers += 1;
-        }
-
-        // make a list of layers where selected stuff is nulled out.
-        // that way we can still insert in the right place without creating a
-        // surprisingly complicated problem
-        // (what if you drag a mixed selection onto one of them? the end? the start? etc)
-        var reorderedLayers = this.doc.layers.map(l => this.selection.includes(l) ? null : l);
-
-        // insert the new layers
-        reorderedLayers.splice(targetIndexInNewLayers, 0, ...this.selection);
-
-        // only NOW can we remove the null placeholders that made our indexes consistent
-        reorderedLayers = reorderedLayers.filter(l => l != null);
-
-        // and we're done
-        this.doc.layers = reorderedLayers;
-
-        // reset dragdrop
-        this.dragDropIndex = -1;
-      }
       this.inputState.releaseFocus(this);
+
+      if (!this.isDragging) {
+        if (e.shiftKey) {
+          if (!this.selection.includes(layer)) {
+            this.selection.push(layer);
+          } else {
+            this.selection.splice(this.selection.indexOf(layer), 1);
+          }
+        } else {
+          this.selection = [layer];
+        }
+      } else if (this.isDragging) {
+        this.isDragging = false;
+
+        if (layer) {
+
+
+          if (this.dragStartedOnLayer == layer) {
+            if (!this.selection.includes(layer)) {
+              this.selection.push(layer);
+            }
+            this.dragDropIndex = -1;
+          } else {
+
+            this.doc.historyPush("Rearrange Layers");
+
+            // if you click dragged from a non-selected layer, we move that instead
+            var layersToMove = this.selection.includes(this.dragStartedOnLayer) ? this.selection : [this.dragStartedOnLayer];
+            // TODO: once this is over, do we select the layer just moved, like photoshop does?
+
+            var targetIndexInNewLayers = this.doc.layers.indexOf(layer);
+            if (this.mouseEventPositionInElementNormalized(e, <Element>e.target).y > 0.5) {
+              targetIndexInNewLayers += 1;
+            }
+
+            // make a list of layers where selected stuff is nulled out.
+            // that way we can still insert in the right place without creating a
+            // surprisingly complicated problem
+            // (what if you drag a mixed selection onto one of them? the end? the start? etc)
+            var reorderedLayers = this.doc.layers.map(l => layersToMove.includes(l) ? null : l);
+
+            // insert the new layers
+            reorderedLayers.splice(targetIndexInNewLayers, 0, ...layersToMove);
+
+            // only NOW can we remove the null placeholders that made our indexes consistent
+            reorderedLayers = reorderedLayers.filter(l => l != null);
+
+            // and we're done
+            this.doc.layers = reorderedLayers;
+
+            // reset dragdrop
+            this.dragDropIndex = -1;
+          }
+        }
+      }
+
+      this.dragStartedOnLayer = null;
+
     }
+  }
+
+  deleteSelectedLayers() {
+    this.doc.historyPush("Delete Layers");
+    this.doc.layers = this.doc.layers.filter(l => !this.selection.includes(l));
+    this.selection = [];
   }
 
   @HostListener('window:keydown', ['$event'])
   keydown(e: KeyboardEvent) {
-
+    //if (this.inputState.hasFocus(this)){
+    if (e.keyCode == 8 || e.keyCode == 46) { // delete or backspace
+      this.deleteSelectedLayers();
+    }
+    //}
   }
 }
