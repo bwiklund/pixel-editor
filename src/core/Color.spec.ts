884d3f2701270f8695464177b522f5a28eda1aa1
Ben Wiklund <ben@daisyowl.com>
2018-09-27 10:56:53 -0700

    color test cleanup

--- src/core/Color.spec.ts
+++ src/core/Color.spec.ts
@@ -2,22 +2,46 @@ import { Color, HSV } from './Color';
 
 
 describe("color", () => {
+  let hexPairs, hsvPairs;
+  
+  beforeEach(() => {
+    hexPairs = [
+      { color: new Color(0, 0, 0), hex: "#000000" },
+      { color: new Color(0, 0, 0), hex: "#000000" },
+      { color: new Color(255, 255, 255), hex: "#ffffff" },
+      { color: new Color(255, 0, 0), hex: "#ff0000" },
+      { color: new Color(0, 0, 255), hex: "#0000ff" },
+      { color: new Color(127, 127, 127), hex: "#7f7f7f" }
+    ];
+
+    hsvPairs = [
+      { color: new Color(0, 0, 0), hsv: new HSV(0, 0, 0) },
+      { color: new Color(255, 0, 0), hsv: new HSV(0, 1, 1) },
+      { color: new Color(255, 255, 0), hsv: new HSV(1 / 6, 1, 1) },
+      { color: new Color(0, 255, 0), hsv: new HSV(2 / 6, 1, 1) },
+      { color: new Color(0, 255, 255), hsv: new HSV(3 / 6, 1, 1) },
+      { color: new Color(0, 0, 255), hsv: new HSV(4 / 6, 1, 1) },
+      { color: new Color(255, 0, 255), hsv: new HSV(5 / 6, 1, 1) },
+    ];
+  });
+
   it("can convert to hex", () => {
-    expect(new Color(0, 0, 0).toHex()).toEqual("#000000");
-    expect(new Color(255, 255, 255).toHex()).toEqual("#ffffff");
-    expect(new Color(255, 0, 0).toHex()).toEqual("#ff0000");
-    expect(new Color(0, 0, 255).toHex()).toEqual("#0000ff");
-    expect(new Color(127, 127, 127).toHex()).toEqual("#7f7f7f");
+    hexPairs.forEach(pair => expect(pair.color.toHex()).toEqual(pair.hex));
+  });
+
+  it("can convert from hex", () => {
+    hexPairs.forEach(pair => expect(Color.fromHex(pair.hex)).toEqual(pair.color));
   });
 
   it("can convert to hsv", () => {
-    expect(new Color(0, 0, 0).toHSV()).toEqual(new HSV(0, 0, 0));
-    expect(new Color(255, 0, 0).toHSV()).toEqual(new HSV(0, 1, 1));
+    hsvPairs.forEach(pair => expect(pair.color.toHSV()).toEqual(pair.hsv));
+  });
 
-    expect(new Color(255, 255, 0).toHSV().h).toBeCloseTo(1/6);
-    expect(new Color(0, 255, 0).toHSV().h).toBeCloseTo(2/6);
-    expect(new Color(0, 255, 255).toHSV().h).toBeCloseTo(3/6);
-    expect(new Color(0, 0, 255).toHSV().h).toBeCloseTo(4/6);
-    expect(new Color(255, 0, 255).toHSV().h).toBeCloseTo(5/6);
+  it("can convert from hsv", () => {
+    hsvPairs.forEach(pair => expect(pair.hsv.toRGB()).toEqual(pair.color));
+  });
+
+  it("rounds incoming values to int", () => {
+    expect(new Color(0.1,0.1,0.1)).toEqual(new Color(0,0,0));
   });
 });
