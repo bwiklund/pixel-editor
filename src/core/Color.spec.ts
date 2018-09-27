554cb978478700a274a67e8496a1fb3d6fc5007c
Ben Wiklund <ben@daisyowl.com>
2018-09-27 10:58:44 -0700

    colors

--- src/core/Color.spec.ts
+++ src/core/Color.spec.ts
@@ -2,28 +2,24 @@ import { Color, HSV } from './Color';
 
 
 describe("color", () => {
-  let hexPairs, hsvPairs;
-  
-  beforeEach(() => {
-    hexPairs = [
-      { color: new Color(0, 0, 0), hex: "#000000" },
-      { color: new Color(0, 0, 0), hex: "#000000" },
-      { color: new Color(255, 255, 255), hex: "#ffffff" },
-      { color: new Color(255, 0, 0), hex: "#ff0000" },
-      { color: new Color(0, 0, 255), hex: "#0000ff" },
-      { color: new Color(127, 127, 127), hex: "#7f7f7f" }
-    ];
+  let hexPairs = [
+    { color: new Color(0, 0, 0), hex: "#000000" },
+    { color: new Color(0, 0, 0), hex: "#000000" },
+    { color: new Color(255, 255, 255), hex: "#ffffff" },
+    { color: new Color(255, 0, 0), hex: "#ff0000" },
+    { color: new Color(0, 0, 255), hex: "#0000ff" },
+    { color: new Color(127, 127, 127), hex: "#7f7f7f" }
+  ];
 
-    hsvPairs = [
-      { color: new Color(0, 0, 0), hsv: new HSV(0, 0, 0) },
-      { color: new Color(255, 0, 0), hsv: new HSV(0, 1, 1) },
-      { color: new Color(255, 255, 0), hsv: new HSV(1 / 6, 1, 1) },
-      { color: new Color(0, 255, 0), hsv: new HSV(2 / 6, 1, 1) },
-      { color: new Color(0, 255, 255), hsv: new HSV(3 / 6, 1, 1) },
-      { color: new Color(0, 0, 255), hsv: new HSV(4 / 6, 1, 1) },
-      { color: new Color(255, 0, 255), hsv: new HSV(5 / 6, 1, 1) },
-    ];
-  });
+  let hsvPairs = [
+    { color: new Color(0, 0, 0), hsv: new HSV(0, 0, 0) },
+    { color: new Color(255, 0, 0), hsv: new HSV(0, 1, 1) },
+    { color: new Color(255, 255, 0), hsv: new HSV(1 / 6, 1, 1) },
+    { color: new Color(0, 255, 0), hsv: new HSV(2 / 6, 1, 1) },
+    { color: new Color(0, 255, 255), hsv: new HSV(3 / 6, 1, 1) },
+    { color: new Color(0, 0, 255), hsv: new HSV(4 / 6, 1, 1) },
+    { color: new Color(255, 0, 255), hsv: new HSV(5 / 6, 1, 1) },
+  ];
 
   it("can convert to hex", () => {
     hexPairs.forEach(pair => expect(pair.color.toHex()).toEqual(pair.hex));
